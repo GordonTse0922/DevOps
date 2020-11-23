196ed48494b705afad4fdd4adb90ba6afa53623f
Gordon <tsehoyin.gordon@gmail.com>
2020-11-23 17:45:33 +0800

    Version 2  (23/11) Done: Todo, main gaming board Next to do: Scripts for the main gaming part

--- Assets/Scripts/ChangeScenes.cs
+++ Assets/Scripts/ChangeScenes.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class ChangeScenes : MonoBehaviour {
+    
+    public static int counter;
+    public static int selected=0;
 
     public void GoToHomePage() {
         SceneManager.LoadScene(0);
@@ -22,12 +26,43 @@ public class ChangeScenes : MonoBehaviour {
         SceneManager.LoadScene(3);
     }
 
-    public void GoToSelectDepolyment() {
+    public void GoToIntroduceDepolyment() {
         SceneManager.LoadScene(4);
     }
 
     public void GoToBigBangIntro(){
+        counter++;
         SceneManager.LoadScene(5);
+    }
+
+    public void GoToCanaryInro(){
+        counter++;
+        SceneManager.LoadScene(6);
+    }
+    public void GoToBlueGreenInro(){
+        counter++;
+        SceneManager.LoadScene(7);
+    }
+
+    public void GoToDeployScene(){
+        SceneManager.LoadScene(8);
+    }
+
+    public void GoToTasks(){
+        SceneManager.LoadScene(9);
+    }
+    public void GoToGame(){
+        SceneManager.LoadScene(10);
+    }
+    
+    public void SelectedBigBang(){
+        selected=1;
+    }
+    public void SelectedCanary(){
+        selected=2;
+    }
+    public void SelectedBlueGreen(){
+        selected=3;
     }
 
     public void Quit() {
