676e42ba287f9fd348db301c3039a26be4d9b89b
Gordon <tsehoyin.gordon@gmail.com>
2020-11-30 13:44:35 +0800

    For Demo

---
+++ Assets/Scripts/Feature_Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class Feature_Timer : MonoBehaviour
+{
+    GameObject gordon;
+    GameObject designing;
+    GameObject feature1;
+    GameObject worker;
+    // Start is called before the first frame update
+    void Awake() {
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
